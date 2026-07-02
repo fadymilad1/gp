a9bcb9939fddda83e63d2a508bc4a40e3ca9c602
fady milad <fadymilad155@gmail.com>
2026-07-03 02:11:11 +0300

    fixing security stuff

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; frame-src 'self' https://www.google.com https://maps.google.com; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: http://localhost:8000 http://*.localhost:8000 http://127.0.0.1:8000 http://192.168.1.17:8000 https://*.openstreetmap.org https://cdnjs.cloudflare.com; connect-src 'self' ws://localhost:3000 ws://*.localhost:3000 ws://127.0.0.1:3000 http://localhost:8000 http://*.localhost:8000 http://127.0.0.1:8000 http://192.168.1.17:8000 https://nominatim.openstreetmap.org; font-src 'self' data: https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self';"
+            value: "default-src 'self'; frame-src 'self' https://www.google.com https://maps.google.com; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: http://localhost:8000 http://*.localhost:8000 http://127.0.0.1:8000 http://192.168.1.17:8000 https://*.railway.app https://*.onrender.com https://*.openstreetmap.org https://cdnjs.cloudflare.com; connect-src 'self' ws://localhost:3000 ws://*.localhost:3000 ws://127.0.0.1:3000 http://localhost:8000 http://*.localhost:8000 http://127.0.0.1:8000 http://192.168.1.17:8000 https://*.railway.app https://*.onrender.com https://*.vercel.app https://nominatim.openstreetmap.org; font-src 'self' data: https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self';"
           },
           {
             key: 'X-Frame-Options',
